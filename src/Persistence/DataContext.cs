c264a0484770d70e64fb2a7befa965c33c3dd3fe
Diego <diego@anzolin.com.br>
2021-04-09 15:29:56 -0300

    Walking Skeleton Part 1 - API

---
+++ src/Persistence/DataContext.cs
@@ -0,0 +1,19 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
